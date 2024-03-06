f70448d6ce4fb6fddc93ca0edf11a52cd2d81a0d
Dmytro Shpringer <shpringerd@gmail.com>
2024-03-06 18:10:38 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+/* === Imports === */
+import { initializeApp } from "firebase/app"
+
+/* === Firebase Setup === */
+const firebaseConfig = {
+  apiKey: "AIzaSyBM1JtWaj4B_RyDqfnl9yqULGf3U0L33Sk",
+  authDomain: "moody-8f7be.firebaseapp.com",
+  projectId: "moody-8f7be",
+  storageBucket: "moody-8f7be.appspot.com"
+}
+
+const app = initializeApp(firebaseConfig)
+
+/* === UI === */
+
+/* == UI - Elements == */
+
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
+
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
+
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("create-account-btn")
+
+/* == UI - Event Listeners == */
+
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
+
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+
+/* === Main Code === */
+
+showLoggedOutView()
+
+/* === Functions === */
+
+/* = Functions - Firebase - Authentication = */
+
+function authSignInWithGoogle() {
+    console.log("Sign in with Google")
+}
+
+function authSignInWithEmail() {
+    console.log("Sign in with email and password")
+}
+
+function authCreateAccountWithEmail() {
+    console.log("Sign up with email and password")
+}
+
+/* == Functions - UI Functions == */
+
+function showLoggedOutView() {
+    hideElement(viewLoggedIn)
+    showElement(viewLoggedOut)
+}
+
+function showLoggedInView() {
+    hideElement(viewLoggedOut)
+    showElement(viewLoggedIn)
+}
+
+function showElement(element) {
+    element.style.display = "flex"
+}
+
+function hideElement(element) {
+    element.style.display = "none"
+}
